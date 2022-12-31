ecbcb41ce8a9723230c27bf538761a699910e533
fady <gamilfady605@gmail.com>
2022-12-31 14:21:58 +0200

    The UI project can provide the connection string to the DataAccessLibrary

--- RelationalDBSolution/SqlServerUI/Program.cs
+++ RelationalDBSolution/SqlServerUI/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// a method that takes a connection string name as a paramter 
+// and fetch the connection string value from json file 
+// so later we can just replace the connectionStringName paramter 
+// to use another data source rather than the local Sql Server DB
+using Microsoft.Extensions.Configuration;
+
+string GetConnString (string connStringKey = "DefaultConnection")
+{
+    // define the builder 
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json");
+
+    // define the config variable
+    var config = builder.Build();
+
+    // get the connection string from the json file and return the result
+    var connStringValue = config.GetConnectionString(connStringKey);
+    return connStringValue;
+}
+
+// test the method
+var connString = GetConnString();
+Console.WriteLine(connString);
